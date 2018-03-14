d6280004c37575fcba04761de539018868afc97b
SDQ\cristian.restituyo <crestituyo@theoreminc.net>
2018-03-14 19:41:14 -0400

    Tested replace navigation function and ajusted paddings.

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -4,10 +4,18 @@ import { Form, Item, Input} from 'native-base';
 
 export default class Home extends Component {
 
+    state = {
+        username:'',
+        password:''
+    };
+
     static navigationOptions = ({ navigation }) => {
         return {
             header: null
         }
+    }
+    onSignInHandler = () =>{
+        this.props.navigation.replace('Home');
     }
     render() {
         return (
@@ -16,18 +24,22 @@ export default class Home extends Component {
                 <Form style={styles.Form}>
                     <Item rounded style={styles.InputItem}>
                         <Input
+                            onChangeText = {(text)=>{this.setState({username:text})}}
                             placeholder="Username"
                             style={styles.Input} />
                     </Item>
                     <Item rounded style={styles.InputItem}>
                         <Input
+                            onChangeText = {(text)=>{this.setState({password:text})}}
                             placeholder="Password"
                             secureTextEntry
                             style={styles.Input} />
                     </Item>
                 </Form>
                 <View style={styles.Buttons}>
-                    <TouchableOpacity style={[styles.Button, styles.ButtonSignIn]}>
+                    <TouchableOpacity
+                         onPressOut = {this.onSignInHandler}
+                         style={[styles.Button, styles.ButtonSignIn]}>
                         <Text style={[styles.ButtonText, styles.ButtonTextSignIn]}>SIGN IN</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.Button, styles.ButtonSignUp]}>
@@ -50,10 +62,10 @@ const styles = StyleSheet.flatten({
     InputItem: {
         marginVertical: 12,
         backgroundColor: 'rgba(255,255,255,0.4)',
-        borderColor: 'rgba(255,255,255,0.4)'
+        borderColor: 'rgba(255,255,255,0.4)',
+        paddingHorizontal: 12,
     },
     Input: {
-        paddingHorizontal: 12,
     },
     Buttons: {
         flexDirection: 'row',
@@ -66,7 +78,7 @@ const styles = StyleSheet.flatten({
         marginHorizontal: 5,
         justifyContent: 'center',
         backgroundColor: 'rgba(0,0,0,0)',
-        borderWidth: 2,
+        borderWidth: 1,
         borderColor: '#000',
         paddingVertical:12
     },
