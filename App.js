f876d138838efe3f5db7b8f16f8375bf8366922d
SDQ\cristian.restituyo <crestituyo@theoreminc.net>
2018-03-15 11:30:00 -0400

    Passed onOpenDrawer throughout screen as a screenProp

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {RootStack} from './src/Routes';
+import { RootStack } from './src/Routes';
+import { Drawer } from 'native-base';
+import SideBar from './src/components/SideBar/SideBar';
 
 
 export default class App extends React.Component {
+  onCloseDrawer = () => {
+    this.drawer._root.close();
+  }
+  onOpenDrawer = () => {
+    this.drawer._root.open()
+  }
   render() {
     return (
-      <View style = {styles.container}>
-        {/* <MainNav title="Header" /> */}
-        <RootStack />
-      </View>
+      <Drawer
+        ref={(ref) => this.drawer = ref}
+        onCloseDrawer={() => this.onCloseDrawer}
+        content={<SideBar />} >
+        <View style={styles.container}>
+          <RootStack
+            screenProps={{ onOpenDrawer: this.onOpenDrawer }} />
+        </View>
+      </Drawer>
     );
   }
 }
